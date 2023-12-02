96866ca161fa1542e344cd30f2dcebcb5901c52c
Farhan Shahid <farhanomarshahid@gmail.com>
2023-12-02 09:14:02 +0600

    tab6

--- src/app/project/project-detail/project-detail.component.ts
+++ src/app/project/project-detail/project-detail.component.ts
@@ -124,10 +124,12 @@ test:boolean=false;
     this.promainmodalcomponent.callChildModalDelete(this.id);
   }
 
+
+  
   ngOnInit() {
     
     // this.loadEmpDetail();
-    // this.fillProjectCmb();
+    this.fillProjectCmb();
     // //child tabs initially will be updated using parent to child @Input()
     // //On emp cmb search child tabs will be updated using this.empdegreecomponent.loadAngularDatatable(); in findbyemployeeid() method
     
